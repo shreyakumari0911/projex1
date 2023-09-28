8cff5cab76bc19bab93c3be650f90af91f48bd17
Rishav-R03 <rishav042023@gmail.com>
2023-09-28 15:29:08 +0530

    adding seller page and backend

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose")
+var connectionUrl = "mongodb://localhost:27017"
+const express = require('express')
+const app = express()
+
+mongoose.connect(connectionUrl,{useNewParser:true,useUnifiedTopology:true},(err)=>{
+    if(err) throw err
+    console.log("Connected")
+})
+
+app.get("/home",(req,res)=>{
+    res.send("Hello")
+})
+
+const port = process.env.PORT || 4000
+app.listen(port,()=>{
+    console.log(`Listening to port ${port}`)
+})
+app.use(express.urlencoded({extended:true}))
+app.set("view engine","ejs")
+app.get("/index",(req,res)=>{
+    res.render("index")
+})
+const UserModel = require("../models/user")
+app.post("/api/user",(req,res)=>{
+
+})
+const SaveUser = new UserModel(req.body)
+SaveUser.save((error,savedUser)=>{
+    if(error) throw error
+    res.json(savedUser)
+})
+
+app.listen(9000,()=>{
+    console.log("listening to port 9000")
+})
